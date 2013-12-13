f5e4e274f359b75f7fe9d7a306f86eb4496f1102
Karl R. Wurst <karl@w-sts.com>
2013-12-13 13:45:35 -0500

    Renames all Bb files to username.ext

--- BbCleanup.py
+++ BbCleanup.py
@@ -14,11 +14,14 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
 
+
+
 # Cleans up assignment files downloaded from Blackboard's gradebook
+# by deleting all .txt files with no student text data or comments
+# and renaming all the remaining files to username.ext
 
 from os import chdir, listdir, curdir
 from os.path import isfile, join
-
 
 # Get command line arguments
 args = str(sys.argv)
@@ -30,11 +33,10 @@ chdir(dir)
 # Get a list of all files (not directories)
 onlyfiles = [ f for f in listdir() if isfile(join(curdir,f)) ]
 
-# Get rid of Bb .txt files that have no student comments
-
-# get list of all .txt files
+# Get the list of all .txt files
 txtfiles = [ f for f in onlyfiles if '.txt' in f) ]
 
+# Get rid of Bb .txt files that have no student text data or comments
 for f in txtfiles:
     file = open(f)
     contents = file.read()
@@ -43,3 +45,16 @@ for f in txtfiles:
        'There is no student submission text data for this assignment.' in contents:
         os.remove(f)
 
+# Get the list of remaining files
+onlyfiles = [ f for f in listdir() if isfile(join(curdir,f)) ]
+
+# Find all the Bb assignment files, which are formatted like:
+#     assignmentname_username_attempt_date_studentfilename.ext
+# Rename the file to username.ext
+for f in onlyfiles:
+    if '_attempt_' in f:
+        first = f.find('_')
+        second_= f.find('_',first+1)
+        extension = f[f.find('.'):]
+        os.rename(f, f[first+1:second] + extension)
+        
