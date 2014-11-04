700b72d6bd171f1408bb6a23f04576bc7d35e91a
Karl R. Wurst <karl@w-sts.com>
2014-11-04 08:07:09 -0500

    Change bbcleanup to function to allow testing, and add test class.

--- bbcleanup.py
+++ bbcleanup.py
@@ -27,52 +27,56 @@ import sys
 import os
 import os.path
 
-# Get directory from command line arguments
-dir = (sys.argv)[1]
-
-# Change directory
-os.chdir(dir)
-
-# Get a list of all files (not directories) (from http://stackoverflow.com/a/3207973)
-onlyfiles = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(os.curdir,f)) ]
-
-# Get rid of spaces in filenames
-for f in onlyfiles:
-    os.rename(f, f.replace(' ', ''))
-
-# Get the list of all .txt files
-txtfiles = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(os.curdir,f)) and '.txt' in f]
-
-# Get rid of Bb .txt files that have no student text data or comments
-#   and rename remaining files to username.txt
-for f in txtfiles:
-    if '_attempt' in f:
-        file = open(f)
-        contents = file.read()
-        file.close()
-        if 'There are no student comments for this assignment' in contents and \
-           'There is no student submission text data for this assignment.' in contents:
-            os.remove(f)
-            print('Deleted', f)
-        else:
-            first = f.find('_')          # location of first underscore
-            second = f.find('_',first+1) # location of second underscore
-            newf = f[first+1:second] + '.txt'
+def bbcleanup():
+    # Get directory from command line arguments
+    dir = (sys.argv)[1]
+    
+    # Change directory
+    os.chdir(dir)
+    
+    # Get a list of all files (not directories) (from http://stackoverflow.com/a/3207973)
+    onlyfiles = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(os.curdir, f)) ]
+    
+    # Get rid of spaces and parentheses in filenames
+    for f in onlyfiles:
+        newf = f.replace(' ', '').replace('(', '').replace(')', '')
+        os.rename(f, newf)
+    
+    # Get the list of all .txt files
+    txtfiles = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(os.curdir, f)) and '.txt' in f]
+    
+    # Get rid of Bb .txt files that have no student text data or comments
+    #   and rename remaining files to username.txt
+    for f in txtfiles:
+        if '_attempt' in f:
+            file = open(f)
+            contents = file.read()
+            file.close()
+            if 'There are no student comments for this assignment' in contents and \
+               'There is no student submission text data for this assignment.' in contents:
+                os.remove(f)
+                print('Deleted', f)
+            else:
+                first = f.find('_')  # location of first underscore
+                second = f.find('_', first + 1)  # location of second underscore
+                newf = f[first + 1:second] + '.txt'
+                os.rename(f, newf)
+                print('Renamed', f, 'to', newf)
+                
+    # Get the list of all non-.txt files
+    nontxtfiles = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(os.curdir, f)) and not '.txt' in f]
+    
+    # Find all the Bb assignment files, which are formatted like:
+    #     assignmentname_username_attempt_datetime_studentfilename.ext
+    # Rename the file to username-studentfilename.ext
+    for f in nontxtfiles:
+        if '_attempt_' in f:
+            first = f.find('_')  # location of first underscore
+            second = f.find('_', first + 1)  # location of second underscore
+            last = f.rfind('_')  # location of last underscore
+            newf = f[first + 1:second] + '-' + f[last + 1:]
             os.rename(f, newf)
             print('Renamed', f, 'to', newf)
-            
-# Get the list of all non-.txt files
-nontxtfiles = [ f for f in os.listdir(dir) if os.path.isfile(os.path.join(os.curdir,f)) and not '.txt' in f]
-
-# Find all the Bb assignment files, which are formatted like:
-#     assignmentname_username_attempt_datetime_studentfilename.ext
-# Rename the file to username-studentfilename.ext
-for f in nontxtfiles:
-    if '_attempt_' in f:
-        first = f.find('_')          # location of first underscore
-        second = f.find('_',first+1) # location of second underscore
-        last = f.rfind('_')          # location of last underscore
-        newf = f[first+1:second] + '-' + f[last+1:]
-        os.rename(f, newf)
-        print('Renamed', f, 'to', newf)
         
+if __name__ == '__main__':
+    bbcleanup()
---
+++ bbcleanupTester.py
@@ -0,0 +1,11 @@
+import unittest
+import bbcleanup
+
+
+class BbcleanupTester(unittest.TestCase):
+
+    def test_empty_test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
