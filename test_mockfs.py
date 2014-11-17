611f3b66b9d0fe3fe98016c39623eab70536f6de
Karl R. Wurst <karl@w-sts.com>
2014-11-17 12:40:08 -0500

    Add licensing

--- test_mockfs.py
+++ test_mockfs.py
@@ -1,6 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (C) 2013-2014 Karl R. Wurst
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+
 import unittest
 import mockfs
-
 
 class MockfsTests(unittest.TestCase):
     
@@ -47,7 +63,6 @@ class MockfsTests(unittest.TestCase):
         self.fs.setDictionary({'afile':''})
         self.fs.chdir('blah/blah/blee/blee')
         self.assertDictEqual({'afile':''}, self.fs.getDictionary())
-
 
 if __name__ == '__main__':
     unittest.main()
